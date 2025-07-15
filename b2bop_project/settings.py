01520935b9d66751f37765d9e776da1e80ddc64c
Harisankar <harisankar@uniqnex360.com>
2025-07-16 02:43:07 +0530

    updated

--- b2bop_project/settings.py
+++ b2bop_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from .env import *
+# from .env import *
 from corsheaders.defaults import default_headers
 import os
 
