2f8fc08837d70620ac3e64bc572f86a67f9abac4
Petchi-7 <petchimuthukumar@uniqnex.com>
2025-08-18 17:00:27 +0530

    Added home endpoint for root URL

--- b2bop_project/urls.py
+++ b2bop_project/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": True,
+        "message": "Backend is running successfully 🚀"
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include("user_management.urls"))
+    path('app/', include("user_management.urls")),
+    path('', home),   # root endpoint
 ]
